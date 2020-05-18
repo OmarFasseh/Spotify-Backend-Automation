6c8486117dc52edbb2ad2366620f3f68b4997b8e
Omar-Fasseh <okomarkhaled@gmail.com>
2020-05-18 15:26:04 +0200

    deploying using comands instead of git push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-git config --global push.default simple 
-git remote add production ssh://Dev@138.91.114.14/~/BackEndScript
-git push production master 
-ssh -i deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript && pm2 start npm -- start --watch --ignore-watch="node_modules" --name ci'
+#git config --global push.default simple 
+#git remote add production ssh://Dev@138.91.114.14/~/BackEndScript
+#git push production master 
+ssh -i deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript/Spotify-Backend-Automation/ && git pull && npm install && pm2 start npm --name ci -- start --watch --ignore-watch="node_modules" --name ci'
 exit
