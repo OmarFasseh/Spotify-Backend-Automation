37ff317a5f54e4530b281fe61fca5d0c04aa5850
Omar-Fasseh <okomarkhaled@gmail.com>
2020-05-18 15:07:01 +0200

    added deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
-ssh Dev@138.91.114.14 -v exit
+git config --global push.default simple 
+git remote add production ssh://Dev@138.91.114.14/~/BackEndScript
+git push production master 
+ssh -i deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript && pm2 start npm -- start --watch --ignore-watch="node_modules" --name ci'
+exit
