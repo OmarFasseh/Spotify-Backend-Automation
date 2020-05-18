d46cb62c863ad37cbbd3eeaa9dd850675966d12c
Omar Fasseh <48892906+OmarFasseh@users.noreply.github.com>
2020-05-18 17:48:24 +0200

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -e
+#set -e
 #git config --global push.default simple 
 #git remote add production ssh://Dev@138.91.114.14/~/BackEndScript
 #git push production master 
-ssh -i deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript/Spotify-Backend-Automation/ && git pull && npm install'
+ssh -i deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript/Spotify-Backend-Automation/ && git pull origin deploy && npm install'
+#now this only deploys the deploy branch
 exit
