abb322f16443b462020082d1664daa9093d75365
Omar Fasseh <48892906+OmarFasseh@users.noreply.github.com>
2020-06-01 00:42:34 +0200

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,5 @@
 #git config --global push.default simple 
 #git remote add production ssh://Dev@138.91.114.14/~/BackEndScript
 #git push production master 
-ssh -i ~/.ssh/deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript/Spotify-Backend-Automation/ && git clean  -d  -f . &&git reset --hard && git pull && npm install && pm2 reload Backend'
+ssh -i ~/.ssh/deploy_rsa Dev@138.91.114.14 'cd ~/BackEndScript/Spotify-Backend-Automation/ && git clean  -d  -f . &&git reset --hard && git pull && cp service-account-file.json ../service-account-file.json && npm install && pm2 reload Backend'
 exit
